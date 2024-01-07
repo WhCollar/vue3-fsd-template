6b74287f11428873f4ddb4bc3967c56a736e71c5
WhCollar <Monserka@gmail.com>
2024-01-07 00:34:06 -0800

    Added router config

---
+++ src/pages/index.ts
@@ -0,0 +1,14 @@
+﻿import { RouteRecordRaw } from 'vue-router';
+import { RouterEnum } from 'shared/model/router';
+import Routing from './index.vue';
+import Main from './main.vue';
+
+export const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: RouterEnum.Main,
+    component: Main,
+  },
+];
+
+export { Routing };
